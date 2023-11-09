90137ec4f85ee895a782eb5a121a34b4d953433d
Ivonne <ivonnebenites2015@gmail.com>
2023-11-09 09:40:30 -0500

    Update file

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,20 +1,20 @@
 # Hash of the Morse symbols and their respective leters
-MORSE_TO_LETTER = {
-  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-  '--..' => 'Z'
-}.freeze
+# MORSE_TO_LETTER = {
+#   '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+#   '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+#   '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+#   '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+#   '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+#   '--..' => 'Z'
+# }.freeze
 
-def decode_char(morse_char)
-   MORSE_TO_LETTER[morse_char]
-end
+# def decode_char(morse_char)
+#    MORSE_TO_LETTER[morse_char]
+# end
 
-def decode_word(morse_word)
-  morse_word.split(/\s{1}/).map { |char| decode_char(char) { |letter| letter } }.join
-end
+# def decode_word(morse_word)
+#   morse_word.split(/\s{1}/).map { |char| decode_char(char) { |letter| letter } }.join
+# end
 
 def decode(morse_message)
   morse_message.strip.split(/\s{3}/).map { |morse_word| decode_word(morse_word) }.join(' ')
